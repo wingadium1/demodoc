a581d139c1a94b07a0086d3e1c00bbab88105f67
Đức Nam Trần <dynamo.foss@gmail.com>
2014-08-14 23:19:38 +0700

    Refactor code

--- app/src/main/java/mhst/dreamteam/SessionMng/LoginActivity.java
+++ app/src/main/java/mhst/dreamteam/SessionMng/LoginActivity.java
@@ -125,9 +125,6 @@ public class LoginActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 }
